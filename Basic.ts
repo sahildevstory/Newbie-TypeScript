08b7dad9eee5bb276b6a6fd157716a4523aa697b
Sahil Khan <sahil.khan44484@gmail.com>
2025-04-29 00:37:12 +0530

    Day-01 with Basics

---
+++ Basic.ts
@@ -0,0 +1,39 @@
+// const x: number = 4;
+// console.log(x);
+
+
+// let Name = ((a:string, b:string) => {
+// console.log('====================================');
+// console.log(a+b);
+// console.log('====================================');
+// })
+
+// Name("Hello", "World")
+
+
+
+//Return  true or false if user is 18+
+// const checkAge = ((Age:number) => {
+//     if(Age >= 18){
+//         console.log('====================================');
+//         console.log('You are 18+ years old');
+//         console.log('====================================');
+//     }else{
+//         console.log('====================================');
+//         console.log('You are not 18+ years old');
+//         console.log('====================================');
+//     }
+// })
+
+// checkAge(20)
+
+//Add to numnbers
+
+function sum (a:number,b:number):number{
+    return a+b
+}
+
+const value = sum(2,3)
+console.log('====================================');
+console.log(value);
+console.log('====================================');
